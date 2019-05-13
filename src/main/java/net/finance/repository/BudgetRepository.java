40f3edf54f43678b35410ec14cdcdddeb082e38b
Alexandre Gomes da Silva <alexandre.gomes.silva@celfocus.com>
2019-05-13 16:43:10 +0100

    Implemented plannedxActual chart

--- src/main/java/net/finance/repository/BudgetRepository.java
+++ src/main/java/net/finance/repository/BudgetRepository.java
@@ -29,7 +29,8 @@ public interface BudgetRepository extends JpaRepository<Budget, Integer> {
 
 	@Query("select new net.finance.dto.report.PeriodValueDto(bp.startDate, bp.endDate, bc.value as plannedValue, sum(coalesce(ex.value, 0)) as actualValue) from BudgetPeriods bp"
 			+ " left join Budget b on bp.budget = b.id" + " left join BudgetCategories bc on bc.budget = b.id"
-			+ " left join Category cat on bc.category = cat.id" + " left join Expense ex on cat.id = ex.category"
+			+ " left join Category cat on bc.category = cat.id"
+			+ " left join Expense ex on cat.id = ex.category and ex.expireAt between bp.startDate and bp.endDate"
 			+ " where cat.id = :categoryId and :now between bp.startDate and bp.endDate "
 			+ " group by bp.idPeriod, bp.startDate, bp.endDate, bc.value" + " order by bp.idPeriod")
 	List<PeriodValueDto> getAggregateValueByDate(Date now, Integer categoryId);

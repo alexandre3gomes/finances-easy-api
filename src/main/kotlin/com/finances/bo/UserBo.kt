e12d779adb2e7b79563399730b6fb322cd185b25
alexandre.silva <alexandre.silva@digitalhub.man>
2021-01-29 10:12:13 +0000

    FE-89: Adjusted get current user

--- src/main/kotlin/com/finances/bo/UserBo.kt
+++ src/main/kotlin/com/finances/bo/UserBo.kt
@@ -6,8 +6,6 @@ import com.finances.mapper.toDTO
 import com.finances.repository.UserRepository
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
 import org.springframework.stereotype.Service
 import java.security.Principal
 
@@ -15,9 +13,8 @@ import java.security.Principal
 class UserBo(private val userRep: UserRepository) {
 
     fun managerUser(principal: Principal): UserDTO {
-        val userInfo = (principal as OAuth2AuthenticationToken).principal as DefaultOidcUser
-        val user: User? = userRep.getUserByUsername(userInfo.preferredUsername)
-        return user?.toDTO() ?: userRep.save(User(userInfo.fullName, userInfo.preferredUsername)).toDTO()
+        val user: User? = userRep.getUserByUsername(principal.name)
+        return user?.toDTO() ?: userRep.save(User("Need to be changed", principal.name)).toDTO()
     }
 
     fun delete(id: Int) {

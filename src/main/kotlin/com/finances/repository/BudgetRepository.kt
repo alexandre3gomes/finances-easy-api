3dbb9103879d72ac28dcd89918a9006c230f5d37
Alexandre Silva <alexandre@youcanbook.me>
2022-09-03 15:16:41 +0100

    Fixed report errors with dates

--- src/main/kotlin/com/finances/repository/BudgetRepository.kt
+++ src/main/kotlin/com/finances/repository/BudgetRepository.kt
@@ -16,7 +16,7 @@ interface BudgetRepository : JpaRepository<Budget, Int> {
     fun getPeriodsByDate(now: LocalDateTime?): BudgetPeriods?
 
     @Query(
-        "select bp.startDate, bp.endDate, bc.value as plannedValue, sum(coalesce(ex.value, 0)) as actualValue from BudgetPeriods bp" +
+        "select bp.startDate as startDate, bp.endDate as endDate, bc.value as plannedValue, sum(coalesce(ex.value, 0)) as actualValue from BudgetPeriods bp" +
             " left join Budget b on bp.budget = b.id" + " left join BudgetCategories bc on bc.budget = b.id" +
             " left join Category cat on bc.category = cat.id" +
             " left join Expense ex on cat.id = ex.category and ex.expireAt between bp.startDate and bp.endDate" +
@@ -26,7 +26,7 @@ interface BudgetRepository : JpaRepository<Budget, Int> {
     fun getValuesByCategories(budgetId: Int?, categoryId: Int?): List<PeriodValueDto>
 
     @Query(
-        "select bp.startDate, bp.endDate, bc.value as plannedValue, sum(coalesce(ex.value, 0)) as actualValue from BudgetPeriods bp" +
+        "select bp.startDate as startDate, bp.endDate as endDate, bc.value as plannedValue, sum(coalesce(ex.value, 0)) as actualValue from BudgetPeriods bp" +
             " left join Budget b on bp.budget = b.id" + " left join BudgetCategories bc on bc.budget = b.id" +
             " left join Category cat on bc.category = cat.id" +
             " left join Expense ex on cat.id = ex.category and ex.expireAt between bp.startDate and bp.endDate" +
